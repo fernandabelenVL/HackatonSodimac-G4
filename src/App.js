625f0fee52ac36a9db023804805de08e6d2dc521
AlejandraMoralesB <a.moralesbarra@gmail.com>
2020-01-06 17:40:38 -0300

    header + formulario

--- src/App.js
+++ src/App.js
@@ -2,39 +2,47 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import SingleTracking from "./Components/SingleTracking";
+import NavBar from "./Components/NavBar";
+import CategoryBar from "./Components/CategoryBar";
+import Order from "./Components/Order";
 
 function App() {
   return (
-      <div className="App">
-            <header className="App-header">
-         <p>HEADER</p>
-      </header>
-
-      
+    <div className="App">
       <Router>
-         <div>
-            <nav>
-               <ul>
-                  <li>
-                     <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                     <Link to="/singletracking">Single Tracking</Link>
-                  </li>
-               </ul>
-            </nav>
+        <header className="App-header">
+          <div className="App">
+            <div className="header">
+              <NavBar />
+              <CategoryBar />
+            </div>
+            <div className="Check">
+              <Order />
+              <Link to="/"></Link>
+            </div>
+          </div>
+        </header>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/singletracking">Single Tracking</Link>
+              </li>
+            </ul>
+          </nav>
 
-            <Switch>
-               <Route exact path="/"  />
-               <Route path="/singletracking">
-                  <SingleTracking/>
-               </Route>
-            </Switch>
-         </div>
+          <Switch>
+            <Route exact path="/" />
+            <Route path="/singletracking">
+              <SingleTracking />
+            </Route>
+          </Switch>
+        </div>
       </Router>
-      
-
-   </div>
+    </div>
   );
 }
 
---
+++ src/Components/CategoryBar.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import "./categoryBar.css";
+
+class CategoryBar extends Component {
+  render() {
+    return (
+      <nav className="category-bar">
+        <ul>
+          <li className="navbar-options">
+            <a href="blank">CONSTRUCCIÓN Y REPARACIÓN</a>
+          </li>
+          <li className="navbar-options">
+            <a href="blank">PISOS, PINTURAS Y TERMINACIONES</a>
+          </li>
+          <li className="navbar-options">
+            <a href="blank">HERRAMIENTAS Y MAQUINARIAS</a>
+          </li>
+          <li className="navbar-options">
+            <a href="blank">BAÑO Y COCINA</a>
+          </li>
+          <li className="navbar-options">
+            <a href="blank">ELECTROHOGAR Y CLIMATIZACIÓN</a>
+          </li>
+          <li className="navbar-options">
+            <a href="blank">MUEBLES Y ORGANIZACIÓN</a>
+          </li>
+          <li className="navbar-options">
+            <a href="blank">DECORACIÓN E ILUMINACIÓN</a>
+          </li>
+          <li className="navbar-options">
+            <a href="blank">AIRE LIBRE, JARDÍN Y PARRILLAS</a>
+          </li>
+          <li className="navbar-options">
+            <a href="blank">AUTOMÓVIL</a>
+          </li>
+          <li className="navbar-options">
+            <a href="blank">PROYECTOS E INSPIRACIÓNa</a>
+          </li>
+          <li className="navbar-options">
+            <a href="blank">SERVICIOS HOGAR</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default CategoryBar;

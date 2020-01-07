f179c338fe8c1c723e8dd0058f6567c8b465bfd8
adrievelyn <adrievelyn@gmail.com>
2020-01-06 23:53:35 -0300

    agregando data.json a ProductsList

--- src/Components/ProductsList.js
+++ src/Components/ProductsList.js
@@ -1,27 +1,48 @@
 import React from 'react';
 import ProductsListCSS from '../Components/ProductsList.css';
+import data from '../data/mock-data'
+
 
 
 class ProductsList extends React.Component {
-  
-
   render() {
-    return (
-    	<div>
-	<ul className='GeneralMenu'>
-		<li className='Imágen'>Imágen</li>
-   		<li className='Fecha de Compra'>Fecha de Compra</li>
-   		<li className='N° de Orden'>N° de Orden</li>
-   		<li className='Tipo de Entrega'>Tipo de Entrega</li>
-		<li className='N° Boleta / Factura'>N° Boleta / Factura</li>
-   		<li className='Monto ($)'>Monto ($)</li>
-   		<li className='Estado de Entrega'>Estado de Entrega</li>
-   		<li className='Detalle'>Detalle</li>
-	</ul>
-		</div>
+  	return (
 
-    	)	
-    };	
+ <table>
+  <tr>
+    <th>Imágen</th>
+    <th>Fecha de Compra</th>
+    <th>N° de Orden</th>
+    <th>Tipo de Entrega</th>
+    <th>N° Boleta / Factura</th>
+    <th>Monto ($)</th>
+    <th>Estado de Entrega</th>
+    <th>Detalle</th>
+  </tr>
+	{
+	        data.products.map((product) => {
+        	return (
+        	  <tr>
+    <td>{product.image}</td>
+    <td>{product.sell_date}</td>
+    <td>{product.order_number}</td>
+     <td>{product.delivery_type}</td>
+    <td>{product.invoce_number}</td>
+    <td>{product.price}</td>
+    <td>{product.status}</td>
+    <td>{product.details}</td>
+
+  </tr>
+
+        	)
+        })	
+	}
+</table> 		
+
+  		
+ 		
+  	)
+  }
 }
 
 

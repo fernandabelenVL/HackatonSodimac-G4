2ba5ae59f144135832ec9afd4f68f2e5841697e3
fernandabelenVL <fernanda.vegaleiva@gmail.com>
2020-01-07 17:42:14 -0300

    se agrega barra de progreso tracking

--- src/Components/SingleTracking.jsx
+++ src/Components/SingleTracking.jsx
@@ -1,15 +1,63 @@
 import React, { useState, useEffect } from "react";
-import "./SingleTracking.css";
+import "react-step-progress-bar/styles.css";
+import { ProgressBar, Step } from "react-step-progress-bar";
 
 //CSS
 import { Container, Row, Col } from "react-bootstrap";
+import "./SingleTracking.css";
+
 
 export default function SingleTracking() {
   return (
     <div>
-      <Container>
-      <p>**BARRA DE PROGRESO**</p>
-      </Container>
-    </div>
+<ProgressBar percent={75}>
+  <Step>
+    {({ accomplished, index }) => (
+      <div
+        className={`indexedStep ${accomplished ? "accomplished" : null}`}
+      >
+        {index + 1}
+      </div>
+    )}
+  </Step>
+  <Step>
+    {({ accomplished, index }) => (
+      <div
+        className={`indexedStep ${accomplished ? "accomplished" : null}`}
+      >
+        {index + 1}
+      </div>
+    )}
+  </Step>
+  <Step>
+    {({ accomplished, index }) => (
+      <div
+        className={`indexedStep ${accomplished ? "accomplished" : null}`}
+      >
+        {index + 1}
+      </div>
+    )}
+  </Step>
+  <Step>
+    {({ accomplished, index }) => (
+      <div
+        className={`indexedStep ${accomplished ? "accomplished" : null}`}
+      >
+        {index + 1}
+      </div>
+    )}
+  </Step>
+  <Step>
+    {({ accomplished, index }) => (
+      <div
+        className={`indexedStep ${accomplished ? "accomplished" : null}`}
+      >
+        {index + 1}
+      </div>
+    )}
+  </Step>
+  
+</ProgressBar>   
+</div>
   );
 }

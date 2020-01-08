942837e11fa1ee0a2679273889a5978b257b5caf
fernandabelenVL <fernanda.vegaleiva@gmail.com>
2020-01-08 12:18:18 -0300

    cambio de logica en pasos de tracking

--- src/Components/SingleTracking.jsx
+++ src/Components/SingleTracking.jsx
@@ -1,63 +1,116 @@
-import React, { useState, useEffect } from "react";
-import "react-step-progress-bar/styles.css";
-import { ProgressBar, Step } from "react-step-progress-bar";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
-//CSS
-import { Container, Row, Col } from "react-bootstrap";
-import "./SingleTracking.css";
+const useStyles = makeStyles((theme) => ({
+   root: {
+      width: "100%"
+   },
+   button: {
+      marginRight: theme.spacing(1)
+   },
+   instructions: {
+      marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(1)
+   }
+}));
 
+function getSteps() {
+   return [
+      "Orden de compra recibida",
+      "Compra confirmada",
+      "Preparando el despacho",
+      "Listo para despacho",
+      "En Ruta",
+      "Entregado"
+   ];
+}
 
-export default function SingleTracking() {
-  return (
-    <div>
-<ProgressBar percent={75}>
-  <Step>
-    {({ accomplished, index }) => (
-      <div
-        className={`indexedStep ${accomplished ? "accomplished" : null}`}
-      >
-        {index + 1}
+function getStepContent(step) {
+   switch (step) {
+      case 0:
+         return "Siguiente...";
+      case 1:
+         return "Sigue sigue...";
+      case 2:
+         return "Terminadooo";
+      case 3:
+         return "Terminadooo";
+      case 4:
+         return "Terminadooo";
+      case 5:
+         return "Terminadooo";
+
+      default:
+         return "Paso Desconocido";
+   }
+}
+
+export default function CustomizedSteppers() {
+   const classes = useStyles();
+   const [activeStep, setActiveStep] = React.useState(1);
+   const steps = getSteps();
+
+   const handleNext = () => {
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+   };
+
+   const handleBack = () => {
+      setActiveStep((prevActiveStep) => prevActiveStep - 1);
+   };
+
+   const handleReset = () => {
+      setActiveStep(0);
+   };
+
+   return (
+      <div className={classes.root}>
+         <Stepper alternativeLabel activeStep={activeStep}>
+            {steps.map((label) => (
+               <Step key={label}>
+                  <StepLabel>{label}</StepLabel>
+               </Step>
+            ))}
+         </Stepper>
+         <div>
+            {activeStep === steps.length ? (
+               <div>
+                  <Typography className={classes.instructions}>
+                     Proceso terminado
+                  </Typography>
+                  <Button onClick={handleReset} className={classes.button}>
+                     Reset
+                  </Button>
+               </div>
+            ) : (
+               <div>
+                  <Typography className={classes.instructions}>
+                     {getStepContent(activeStep)}
+                  </Typography>
+                  <div>
+                     <Button
+                        disabled={activeStep === 0}
+                        onClick={handleBack}
+                        className={classes.button}
+                     >
+                        Back
+                     </Button>
+                     <Button
+                        variant='contained'
+                        color='primary'
+                        onClick={handleNext}
+                        className={classes.button}
+                     >
+                        {activeStep === steps.length - 1 ? "Finish" : "Next"}
+                     </Button>
+                  </div>
+               </div>
+            )}
+         </div>
       </div>
-    )}
-  </Step>
-  <Step>
-    {({ accomplished, index }) => (
-      <div
-        className={`indexedStep ${accomplished ? "accomplished" : null}`}
-      >
-        {index + 1}
-      </div>
-    )}
-  </Step>
-  <Step>
-    {({ accomplished, index }) => (
-      <div
-        className={`indexedStep ${accomplished ? "accomplished" : null}`}
-      >
-        {index + 1}
-      </div>
-    )}
-  </Step>
-  <Step>
-    {({ accomplished, index }) => (
-      <div
-        className={`indexedStep ${accomplished ? "accomplished" : null}`}
-      >
-        {index + 1}
-      </div>
-    )}
-  </Step>
-  <Step>
-    {({ accomplished, index }) => (
-      <div
-        className={`indexedStep ${accomplished ? "accomplished" : null}`}
-      >
-        {index + 1}
-      </div>
-    )}
-  </Step>
-  
-</ProgressBar>   
-</div>
-  );
+   );
 }

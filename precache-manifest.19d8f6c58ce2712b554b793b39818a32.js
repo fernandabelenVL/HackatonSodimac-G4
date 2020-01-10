afb58ecb0d33fbb89c77af97472b9226e51a1dd0
fernandabelenVL <fernanda.vegaleiva@gmail.com>
2020-01-10 07:17:09 -0300

    Updates

---
+++ precache-manifest.19d8f6c58ce2712b554b793b39818a32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1377444303711c08bc74b3f36150b23c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f10da5a104e028aa5abb",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "0023bce93aa43ec2c351",
+    "url": "/static/css/main.e14705f3.chunk.css"
+  },
+  {
+    "revision": "f10da5a104e028aa5abb",
+    "url": "/static/js/2.228253b8.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/static/js/2.228253b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0023bce93aa43ec2c351",
+    "url": "/static/js/main.fc43ba00.chunk.js"
+  },
+  {
+    "revision": "3ab24851f716a12d5580",
+    "url": "/static/js/runtime-main.b1f47b0f.js"
+  }
+]);
